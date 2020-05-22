6ef108b2a55e08b905ec396a96464dd69af367f9
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-22 04:25:42 +0000

    Fix style pep8 camel case

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ I love geometry! """
 
 
-class square():
+class Square():
     """ class """
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.height * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ perimeter """
         return (self.width * 2) + (self.height * 2)
 
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
     """ exec """
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
